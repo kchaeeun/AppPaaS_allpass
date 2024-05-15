2887f286ca36d648992150a3a5a1c80d5ff8b1f4
kchaeeun <oceanball38@naver.com>
2024-05-15 18:24:07 +0900

    프로젝트 생성

---
+++ src/main/java/appPaaS/allpass/AllpassApplication.java
@@ -0,0 +1,13 @@
+package appPaaS.allpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllpassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/appPaaS/allpass/AllpassApplicationTests.java
@@ -0,0 +1,13 @@
+package appPaaS.allpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
